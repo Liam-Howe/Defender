23609b26fcf3d92251eb09ce01c3433bc6ab7103
Liam-Howe <laaim1995@gmail.com>
2017-01-04 14:29:39 +0000

    camera bouns + player bounds , player movement with acceleration/decceleration

--- include/Play.hpp
+++ include/Play.hpp
@@ -13,16 +13,21 @@ private:
 	//Player porperties
 	Player * _player;
 	sf::Texture _playerTexture;
-
+	sf::View _playerView;
 
 	//backgorund properties
 	sf::Texture _backgorundTexture;
 	sf::Sprite _backgroundSprite;
-
+	float gameHeight;
+	float gameWidth;
+	float cameraoffset;
+	float WidthOffset;
+	float heightOffset;
 public:
 
 	virtual void draw();
-	
+	void updateCamera();
+	void wrapAround();
 	virtual void update();
 	virtual void handleInput();
 
--- include/Player.h
+++ include/Player.h
@@ -7,7 +7,12 @@ public :
 	Player(sf::Vector2f m_Pos,sf::Vector2f _Vel,sf::Texture _Tex);
 	~Player();
 	sf::Sprite getSprite();
+	sf::Vector2f getPosition();
 	void move(sf::Vector2f speed);
+	void setPosition(sf::Vector2f _tempPos);
+	void update();
+	bool  isdecelerating();
+	void setdecelerating(bool value);
 private :
 
 	sf::Vector2f m_Pos;
@@ -15,4 +20,5 @@ private :
 	sf::Vector2f m_accel;
 	sf::Texture m_Tex;
 	sf::Sprite m_Sprite;
+	bool m_decelerate;
 };
