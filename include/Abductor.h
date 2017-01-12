5669997303c64840c5397cb2f19755d76299c4f8
Liam-Howe <laaim1995@gmail.com>
2017-01-12 16:07:11 +0000

    flocking

--- include/Abductor.h
+++ include/Abductor.h
@@ -17,13 +17,29 @@ public :
 	void seek(sf::Vector2f targetPos);
 	sf::Vector2f Normalise(sf::Vector2f velocity);
 	void movement(sf::Vector2f abductorPos);
-
+	sf::Vector2f flock(Abductor& _abductor);
+	 sf::Vector2f alignment(Abductor&_abductor);
+	sf::Vector2f seperation(Abductor& _abductor);
+	sf::Vector2f cohesion(Abductor _abductor);
+	sf::Vector2f subTwoVector(sf::Vector2f,sf::Vector2f);
+	sf::Vector2f Abductor::divScalar(float);
+	sf::Vector2f addVector(sf::Vector2f);
+	sf::Vector2f getVelocity();
+	sf::Vector2f mulScalar(float s);
+	sf::Vector2f sum;
+	sf::Vector2f limit(double max);
+	
+	
 private : 
 	sf::Vector2f m_pos;
 	sf::Vector2f m_vel;
+	sf::Vector2f diff;
+	sf::Vector2f steer;
 	sf::Texture m_tex;
 	sf::Sprite m_sprite;
 	float m_health;
+	float maxSpeed;
+	float maxForce;
 	bool m_flock;
 	bool m_seek;
 
--- src/Abductor.cpp
+++ src/Abductor.cpp
@@ -15,6 +15,105 @@ void Abductor::update()
 {
 
 }
+sf::Vector2f Abductor::flock(Abductor& _abductor)
+{
+
+	sf::Vector2f _alignment = alignment(_abductor);
+	sf::Vector2f _seperation = seperation(_abductor);
+	sf::Vector2f _cohesion = cohesion(_abductor);
+
+
+
+}
+sf::Vector2f Abductor::getVelocity()
+{
+	return m_vel;
+}
+sf::Vector2f Abductor::mulScalar(float s)
+{
+
+	sum.x *= s;
+	sum.y *= s;
+	return sum;
+}
+
+
+
+sf::Vector2f Abductor::alignment(Abductor&_abductor)
+{
+	float neighbordist = 50;
+	sum = sf::Vector2f(0, 0);
+	int count = 0;
+
+	float d = sqrt(((m_pos.x - _abductor.getPosition().x) * (m_pos.x - _abductor.getPosition().x)) + ((m_pos.y - _abductor.getPosition().y) * (m_pos.y - _abductor.getPosition().y)));
+	if (d > 0 && (d < neighbordist))
+	{
+		sum = addVector(_abductor.getVelocity());
+		count++;
+	}
+	if (count > 0)
+	{
+		sum = divScalar((float)count);
+		Normalise(sum);
+		sum = mulScalar(maxSpeed);
+		sf::Vector2f steer;
+		steer = subTwoVector(sum, m_vel);
+		steer = limit(maxForce);
+
+
+	}
+}
+sf::Vector2f Abductor::limit(double max)
+{
+	double size = magnitude();
+
+	if (size > max)
+	{
+		set(x / size, y / size);
+	}
+	return steer;
+}
+sf::Vector2f Abductor::seperation(Abductor& _abductor)
+{
+	float desiredseparation = 20;
+	steer = sf::Vector2f(0, 0);
+	int count = 0;
+
+	float d = sqrt(((m_pos.x - _abductor.getPosition().x) * (m_pos.x - _abductor.getPosition().x)) + ((m_pos.y - _abductor.getPosition().y) * (m_pos.y - _abductor.getPosition().y)));
+	if (d > 0 && (d < desiredseparation))
+	{
+		diff = sf::Vector2f(0, 0);
+		diff = subTwoVector(m_pos, _abductor.getPosition());
+		Normalise(diff);
+		diff = divScalar(d);
+		steer = addVector(diff);
+
+	}
+	return steer;
+}
+sf::Vector2f Abductor::subTwoVector(sf::Vector2f m_pos,sf::Vector2f a_pos)
+{
+	m_pos.x -= a_pos.x;
+	m_pos.y -= a_pos.y;
+}
+
+sf::Vector2f Abductor::divScalar(float d)
+{
+	diff.x /= d;
+	diff.y /= d;
+
+}
+sf::Vector2f Abductor::addVector(sf::Vector2f v)
+{
+	steer.x += v.x;
+	steer.y += v.y;
+}
+sf::Vector2f Abductor::cohesion(Abductor _abductor)
+{
+
+	
+}
+
 void Abductor::movement(sf::Vector2f targetPos)
 {
 	float Dist = sqrt(((m_pos.x - targetPos.x) * (m_pos.x - targetPos.x)) + ((m_pos.y - targetPos.y) * (m_pos.y - targetPos.y)));
