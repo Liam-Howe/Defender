2610733ba834d4f73b4f339c343cc1e0a58b20af
Liam-Howe <laaim1995@gmail.com>
2017-01-13 19:10:45 +0000

    setting up thor

--- include/Play.hpp
+++ include/Play.hpp
@@ -12,13 +12,26 @@
 #include "AlienNest.h"
 #include "Abductor.h"
 #include "CollisionManager.h"
+#include "../Dependencies/Thor/include/Particles.hpp"
+#include "../Dependencies/Thor/include/Particles/Affectors.hpp"
+#include "../Dependencies/Thor/include/Particles/EmissionInterface.hpp"
+#include "../Dependencies/Thor/include/Particles/Emitters.hpp"
+#include "../Dependencies/Thor/include/Particles/ParticleSystem.hpp"
+#include "../Dependencies/Thor/include/Vectors/VectorAlgebra3D.hpp"
+#include "../Dependencies/Thor/include/Particles/ParticleSystem.hpp"
+#include "../Dependencies/Thor/include/Math/Distributions.hpp"
+
 
 class Play : public GameState
 {
 private:
 	
 	CollisionManager _collisionManager;
-
+	thor::ParticleSystem system;
+	sf::Clock Particleclock;
+	thor::UniversalEmitter Snowemitter1;
+	thor::UniversalEmitter Snowemitter2;
+	thor::UniversalEmitter Snowemitter3;
 	//Player porperties
 	Player * _player;
 	sf::Texture _playerTexture;
